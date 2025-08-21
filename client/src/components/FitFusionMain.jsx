847a7705eff3f4b843cc17cef3b1cd4c7bb90b39
Satish Kollu <satishkollu954@gmail.com>
2025-08-21 15:08:51 +0530

    changes done

--- client/src/components/FitFusionMain.jsx
+++ client/src/components/FitFusionMain.jsx
@@ -4,22 +4,27 @@ import "./FitFusionMain.css";
 export function FitFusionMain() {
   return (
     <main className="mt-1">
-      <div className="men-fashion">
+      <div className="men-fashion fashion-card">
+        <Link to="/men" className="fashion-overlay"></Link>
         <div className="main-title">Men</div>
         <Link to="/men" className="btn btn-light">
-          shop Men <span className="bi bi-arrow-right"></span>
+          Shop Men <span className="bi bi-arrow-right"></span>
         </Link>
       </div>
-      <div className="women-fashion">
+
+      <div className="women-fashion fashion-card">
+        <Link to="/women" className="fashion-overlay"></Link>
         <div className="main-title">Women</div>
         <Link to="/women" className="btn btn-light">
-          shop Women <span className="bi bi-arrow-right"></span>
+          Shop Women <span className="bi bi-arrow-right"></span>
         </Link>
       </div>
-      <div className="kids-fashion">
+
+      <div className="kids-fashion fashion-card">
+        <Link to="/kids" className="fashion-overlay"></Link>
         <div className="main-title">Kids</div>
         <Link to="/kids" className="btn btn-light">
-          shop Kids <span className="bi bi-arrow-right"></span>
+          Shop Kids <span className="bi bi-arrow-right"></span>
         </Link>
       </div>
     </main>
